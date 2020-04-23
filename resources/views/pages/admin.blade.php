aaaf66ecd390788809595ce54045a0a30d56c23d
Chaloulou75 <c.jeandey@gmail.com>
2020-04-23 10:57:49 -0300

    admin +

--- resources/views/pages/admin.blade.php
+++ resources/views/pages/admin.blade.php
@@ -10,10 +10,26 @@
             <h1>Admin dashboard</h1>
     </div>
 	<ul class="">
-		<li class="py-2 px-4 m-2">- Lien programas create : <a class="text-sm text-julien-red not-italic" href="{{ action('ProgrammeController@create') }}"> Crear un programa</a></li>
-		<li class="py-2 px-4 m-2">- Lien testimonios create: 
+		<li class="py-2 px-4 m-2">- Lien vers programas/create : 
+			<a class="text-sm text-julien-red not-italic" href="{{ action('ProgrammeController@create') }}"> Crear un programa</a></li>
+
+		<li class="py-2 px-4 m-2">- Lien vers testimonios/create: 
 			<a class="text-sm text-julien-red not-italic" href="{{ action('TestimoniosController@create') }}">Crear un testimonio</a></li>
-		<li class="py-2 px-4 m-2">Tu veux d'autres choses ici? dis moi bébé.... liens vers paiements? etc...</li>
+
+		<li class="py-2 px-4 m-2"> - Lien vers Paiement - 6 sesiones: 
+			<a class="text-sm text-julien-red not-italic" href="{{ action('PaymentController@plan6sesiones') }}"> Paiement - 6 sesiones:</a></li>
+
+		<li class="py-2 px-4 m-2">- Lien vers Paiement - 12 sesiones: 
+			<a class="text-sm text-julien-red not-italic" href="{{ action('PaymentController@plan12sesiones') }}">Paiement - 12 sesiones:</a></li>
+
+		<li class="py-2 px-4 m-2">- Lien vers confirmation de paiement pour le plan 6 sessions: 
+			<a class="text-sm text-julien-red not-italic" href="{{ action('PaymentController@plan6confirmado') }}">confirmado 6</a></li>
+
+		<li class="py-2 px-4 m-2">- Lien vers confirmation de paiement pour le plan 12 sessions: 
+			<a class="text-sm text-julien-red not-italic" href="{{ action('PaymentController@plan12confirmado') }}">confirmado 12</a></li>
+
+		<li class="py-2 px-4 m-2">- Lien vers Paiement rechazado: 
+			<a class="text-sm text-julien-red not-italic" href="{{ action('PaymentController@rechazado') }}">Rechazado</a></li>
 	</ul>
 </div>
 
