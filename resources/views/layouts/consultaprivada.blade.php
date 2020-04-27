f2c3dfdf383a0cbfc0776fae2f1f866a2ba85e10
Chaloulou75 <c.jeandey@gmail.com>
2020-04-27 12:12:12 -0300

    consultaprivadasuite

--- resources/views/layouts/consultaprivada.blade.php
+++ resources/views/layouts/consultaprivada.blade.php
@@ -1,38 +1,41 @@
-<div class="text-white bg-julien-gris border-b-1 py-4 my-4">
-	<h1 class="w-full font-black text-3xl text-white text-center uppercase bg-julien-gris border-b-1 py-6">
-                Consulta Privada
-    </h1> 
+<div class="bg-white pb-4 mt-12">
+	<div class="text-julien-gris bg-gray-300 border-b-1 py-4 px-2 mb-4">
+		<h1 class="w-full font-black text-3xl text-center uppercase border-b-1 py-6">
+	                Consulta Privada
+	    </h1> 
 
-	<div class="w-full flex flex-col md:flex-row mx-auto justify-center items-center ">
-		<div class="w-full md:w-1/3 text-center">
-			<p class="font-medium text-lg"> 1. Completa el formulario.</p>	
-		</div>
+		<div class="w-full flex flex-col md:flex-row mx-auto justify-center items-center ">
+			<div class="w-full md:w-1/3 text-center">
+				<p class="font-semibold text-xl"> 1. Completa el formulario.</p>	
+			</div>
 
-		<div class="w-full md:w-1/3 text-center">
-			<p class="font-medium text-lg"> 2. Vidéo llamada de 30 min por WhatsApp.</p>		
-		</div>
+			<div class="w-full md:w-1/3 text-center">
+				<p class="font-semibold text-xl"> 2. Vidéo llamada de 30 min por WhatsApp.</p>		
+			</div>
 
-		<div class="w-full md:w-1/3 text-center">
-			<p class="font-medium text-lg"> 3. Recibe tu rutina en PDF con videos.</p>		
-				
+			<div class="w-full md:w-1/3 text-center">
+				<p class="font-semibold text-xl"> 3. Recibe tu rutina en PDF con videos.</p>		
+					
+			</div>
 		</div>
 	</div>
-</div>
-<div class="w-full md:w-2/3 flex flex-col md:flex-row mx-auto justify-center items-center ">
-		<div class="w-full md:w-1/3 text-center mx-auto my-4">
-			<img src="/img/fotosourire.jpeg" class="rounded overflow-hidden object-contain h-auto w-full mx-auto" alt="sourire">	
-		</div>
+	<div class="w-full md:w-2/3 flex flex-col md:flex-row mx-auto justify-center items-center">
+			<div class="w-full md:w-1/3 text-center mx-auto my-4">
+				<img src="/img/fotosourire.jpeg" class="rounded overflow-hidden object-contain h-64 w-full mx-auto" alt="sourire">	
+			</div>
 
-		<div class="w-full md:w-2/3 text-center not-italic mx-auto px-8">
-			<p class="font-medium text-lg my-"> Vamos a definir tus capacidades, lo que podés hacer y lo que NO podés hacer.</br>Respecto a tus problemas, te voy a proponer la mejor rutina ¡PARA VOS!</p>
-			<p class="leading-normal font-medium uppercase text-lg my-4 mx-auto">
-                1500 AR$ / 22 US$
-            </p>		
-		</div>
-</div>
+			<div class="w-full md:w-2/3 text-center text-gray-900 not-italic mx-auto px-8">
+				<p class="font-medium text-base my-4"> Vamos a definir tus capacidades, lo que podés hacer y lo que NO podés hacer.</br>Respecto a tus problemas, te voy a proponer la mejor rutina ¡PARA VOS!</p>
+				<p class="leading-normal font-medium uppercase text-lg my-4 mx-auto">
+	                1500 AR$ / 22 US$
+	            </p>		
+			</div>
+	</div>
 
-<div class="text-julien-gris mb-8 text-center items-center my-6">
-	<h3 class="uppercase not-italic font-semibold mx-auto py-2 px-4">COMPLETA EL FORMULARIO</h3>
-	<a href="{{route('contact.index')}}"><button class="py-2 px-6 bg-julien-red text-white font-semibold tracking-widest uppercase border-2 border-white rounded shadow hover:shadow-lg transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110" type="button">Hace clic aca</button></a>
+	<div class="text-julien-gris mb-8 text-center items-center my-6">
+		<h3 class="uppercase not-italic font-semibold mx-auto py-2 px-4">COMPLETA EL FORMULARIO</h3>
+		<a href="{{route('contact.index')}}"><button class="py-2 px-6 bg-julien-red text-white font-semibold tracking-widest uppercase border-2 border-white rounded shadow hover:shadow-lg transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110" type="button">Hace clic aca</button></a>
+	</div>
+
 </div>			
 
