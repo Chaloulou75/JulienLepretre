27cfb8822271942d03d2e0c28907d5bc08a88e9a
Chaloulou75 <c.jeandey@gmail.com>
2020-04-27 21:27:49 -0300

    points -

--- resources/views/layouts/consultaprivada.blade.php
+++ resources/views/layouts/consultaprivada.blade.php
@@ -8,15 +8,15 @@
 	 
 	<div class="w-full flex flex-col md:flex-row mx-auto border-b-1 bg-bandeau-gris text-white py-2 px-2">
 		<div class="w-full md:w-1/3 text-center font-medium text-lg not-italic">
-			 1. Completa el formulario.	
+			 1. Completa el formulario	
 		</div>
 
 		<div class="w-full md:w-1/3 text-center font-medium text-lg not-italic">
-			2. Vidéo llamada por WhatsApp.		
+			2. Vidéo llamada por WhatsApp		
 		</div>
 
 		<div class="w-full md:w-1/3 text-center font-medium text-lg not-italic">
-			3. Recibe tu rutina en PDF con videos.
+			3. Recibe tu rutina en PDF con videos
 		</div>
 	</div>
 
