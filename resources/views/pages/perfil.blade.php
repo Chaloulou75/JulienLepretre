6b3e4d0e06d00b6c662ed9c5a872f8bf2ca1177a
Chaloulou75 <c.jeandey@gmail.com>
2020-04-30 21:47:48 -0300

    debut perfil2

--- resources/views/pages/perfil.blade.php
+++ resources/views/pages/perfil.blade.php
@@ -46,19 +46,17 @@
                 <div class="flex-1 px-4 py-2 text-center">
                     <i class="fas fa-clinic-medical fa-4x text-center py-2 items-center"></i>
                     <h2 class="text-lg font-semibold py-2 my-2">Segundo Consultorio</h2>
-                    <p class=" text-justify">Desde 2019 trabajo en mi nuevo consultorio en Buenos Aires.</p>
+                    <p class="text-justify">Desde 2019 trabajo en mi nuevo consultorio en Buenos Aires.</p>
                 </div>
                 <div class="flex-1 px-4 py-2 text-center">
                     <i class="fas fa-chalkboard-teacher fa-4x text-center py-2 items-center"></i>
                     <h2 class="text-lg font-semibold py-2 my-2">Docente</h2>
-                    <p class=" text-justify">Enseno clases sobre el movimiento para una Escuela de osteopatia francesa y para formaciones de yoga.</p2>
+                    <p class="text-justify">Enseno clases sobre el movimiento para una Escuela de osteopatia francesa y para formaciones de yoga.</p2>
                 </div>
                    
         </div>
 
-
-
-        <div class="flex flex-col md:flex-row items-center justify-around m-2">
+        <div class="flex flex-col md:flex-row items-center justify-around my-2">
             <div class="w-full md:w-1/3 m-4">
             	<img src="/img/profilmi.jpg" class="transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110 h-48 w-48 md:h-full md:w-full mx-auto md:mx-0 rounded-full hover:shadow-lg">
             </div>
