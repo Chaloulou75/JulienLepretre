27a9f98d21d09d65f190d53c7d98e9b249d3b3b1
Chaloulou75 <c.jeandey@gmail.com>
2020-05-03 17:08:38 -0300

    prog update grid fav

--- resources/views/pages/programas/show.blade.php
+++ resources/views/pages/programas/show.blade.php
@@ -4,111 +4,115 @@
     
     <div class="mb-4">
 
-        <h1 class="w-full font-black text-3xl text-center uppercase py-8 ">
-                {{ $programme->title }}
+        {{-- titre --}}
+
+        <h1 class="w-full font-black text-3xl text-center not-italic tracking-loose uppercase py-4 ">
+                {{ $programme->title }} </br>               
+                <i class="fas fa-star fa-sm text-yellow-400 pl-1"></i>
+                <i class="fas fa-star fa-sm text-yellow-400 pl-1"></i>
+                <i class="fas fa-star fa-sm text-yellow-400 pl-1"></i>
+                <i class="fas fa-star fa-sm text-yellow-400 pl-1"></i>
+                <i class="fas fa-star fa-sm text-yellow-400 "></i>
         </h1> 
+
+        {{-- premier block --}}
 
         <div class="w-full md:w-3/4 mx-auto flex flex-col-reverse md:flex-row items-center justify-around text-justify mb-2" >
 
-            <!--Left Col -->
-            <div class="flex flex-col w-full h-full lg:w-1/2 mx-auto items-center">
-                <iframe width="420" height="345" src="http://www.youtube.com/embed/{{$programme->lienYoutube}}" allowfullscreen>
+            <!--Left Col youtube-->
+            <div class="flex flex-col w-full h-full md:w-1/2 mx-auto items-center">
+                <iframe class="w-full mx-auto" height="345" src="http://www.youtube.com/embed/{{$programme->lienYoutube}}" allowfullscreen>
                 </iframe>            	
             </div>
 
             <!--Right Col-->
-            <div class="w-full lg:w-1/2 md:py-2 text-center items-center">
+            <div class="w-full md:w-1/2 md:py-2 text-center {{-- items-center --}} mx-auto">
                 
-                <div class="pt-4 md:mt-2 m-2">
-                    <h3 class="font-bold uppercase">
-                    Programa
-                    </h3>
-                    <h3 class="uppercase not-italic font-semibold tracking-loose ">
-                        {{ $programme->title }}
-                    </h3>
-                </div>
-
-                <p class="py-2 mx-auto text-center mb-2">
-                        <i class="fas fa-star fa-fw text-yellow-400 pl-1"></i>
-                        <i class="fas fa-star fa-fw text-yellow-400 pl-1"></i>
-                        <i class="fas fa-star fa-fw text-yellow-400 pl-1"></i>
-                        <i class="fas fa-star fa-fw text-yellow-400 pl-1"></i>
-                        <i class="fas fa-star fa-fw text-yellow-400 pl-1"></i>
-                    </p>
-
-                <p class="whitespace-pre-line leading-normal text-center font-medium mb-2 mx-auto px-8">
+                <p class="whitespace-pre-line leading-normal md:mx-16 md:text-left font-medium mb-2 px-8">
                     {{ $programme->descriptionCourte }}
                 </p>
 
-                <div class="grid grid-cols-4 mb-2">
-                    <div></div>
-                    <div>
+                <div class="grid grid-cols-6 my-4">                 
+                  <div class="col-span-3 md:col-span-1 md:col-start-2">
                         <i class="fas fa-film fa-lg fa-2x"></i>
                         <p class="text-xs"> Videos</p>
-                    </div> 
-                    <div>
+                  </div>
+                  <div class="col-span-3 md:col-span-2">
                         <i class="fas fa-infinity fa-lg fa-2x"></i>
                         <p class="text-xs" >Lifetime access</p>
-                    </div>
-                    <div></div>
+                  </div>
                 </div>
 
-                <h3 class="leading-normal font-bold uppercase text-lg not-italic mb-4 mx-auto">
+                <div class="md:mx-16 md:text-left">
+                    <h3 class="leading-normal font-bold uppercase text-lg not-italic mb-4 md:mx-16 md:text-left mx-auto px-8">
                          {{ floatval($programme->prixPesos) }} <span class="text-sm font-sm text-gray-600">AR$</span> / {{ floatval($programme->prixDollar) }} <span class="text-sm font-sm text-gray-600">US$</span>
-                </h3>
- 
-                <a href="{{ url($programme->lienTiendup) }}" class="mx-auto"><button class="bg-julien-red text-white font-semibold tracking-widest uppercase border border-white rounded shadow hover:shadow-lg py-2 px-4 mx-auto transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110" type="button">Comprar</button></a>
-                <p class="pt-1 text-xs">Pagarlo con MercadoPago o Paypal</p>
+                    </h3> 
+
+                    <a href="{{ url($programme->lienTiendup) }}" class="mx-auto px-8"><button class="bg-julien-gris hover:bg-julien-red text-white font-semibold tracking-widest uppercase border border-white rounded shadow hover:shadow-lg py-2 px-8 mx-auto transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110" type="button">Comprar</button></a>
+                    <p class="py-2 text-xs px-8">Pagarlo con MercadoPago o Paypal</p>
+                </div>
+                 
+                
 
             </div>
         </div>
-        
-		<div class="w-full text-center mb-2">
-        	<h2 class="w-full font-black text-3xl text-white bg-julien-gris border-b-1 text-center uppercase py-8">El programa</h2>
-        </div>
 
-        <div class="w-full md:w-5/6 mx-auto flex flex-col md:flex-row items-center justify-around text-justify mb-2">
-            <div class="flex flex-col w-full h-full lg:w-1/2 px-2 my-4">
-                <p class="whitespace-pre-wrap leading-normal text-base text-justify items-center mx-auto px-4">{{$programme->descriptionComplete}}</p>
 
-                <a href="{{url($programme->lienTiendup) }}" class="mx-auto"><button class=" bg-julien-red text-white font-semibold tracking-widest uppercase border border-white rounded shadow hover:shadow-lg py-2 px-4 mx-auto transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110" type="button">Pedir Ahora</button></a>
+        {{-- Deuxieme block --}}
+
+        <div class="w-full mx-auto flex flex-col md:flex-row items-center text-center mb-4">
+
+            <div class="flex flex-col w-full lg:w-1/2 mx-auto my-4">
+
+                <h2 class="w-full font-black text-3xl uppercase tracking-widest my-2 md:mx-16 md:text-left">El programa</h2>
+
+                <p class="whitespace-pre-wrap leading-normal text-base my-4 md:mx-16 md:text-left items-center">{{$programme->descriptionComplete}}</p>
+                
+                <div class="md:mx-16 md:text-left my-4">
+                    <a href="{{url($programme->lienTiendup) }}"><button class=" bg-julien-gris hover:bg-julien-red text-white font-semibold tracking-widest uppercase border border-white rounded shadow hover:shadow-lg py-2 px-8 mx-auto transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110" type="button">Pedir Ahora</button></a>
+                </div>
             </div>
 
-            <div class="w-full lg:w-1/2 md:py-2 text-center">
+            <div class="w-full lg:w-1/2">
 
                 <img src="{{ url($programme->imageurl)}}" class="fill-current object-cover w-full mx-auto" alt="image">
             </div>
         </div>
 
-        <div class="w-full text-center mb-2">
-            <h2 class="w-full font-black text-3xl text-white bg-julien-gris border-b-1 text-center uppercase py-8">Adentro del programa</h2>
-        </div>    
-
-    	<div class="w-full md:w-5/6 mx-auto flex flex-col-reverse md:flex-row items-center mb-4">
+        {{-- 3eme block --}}
+   
+    	<div class="w-full mx-auto flex flex-col-reverse md:flex-row items-center text-center mb-4">
 
             <!--Left Col -->
-            <div class="flex flex-col w-full lg:w-1/2 justify-center border border-white rounded-lg overflow-hidden items-start mb-4">
+            <div class="flex flex-col w-full lg:w-1/2 justify-center border border-white rounded-lg overflow-hidden mb-4">
 
 				<img src="{{ url($programme->image2url) }}" class="fill-current object-cover w-full mx-auto" alt="image">
             	                
             </div>
 
             <!--Right Col-->
-            <div class="w-full lg:w-1/2 text-center mb-4">
+            <div class="w-full lg:w-1/2 text-center mx-auto mb-4">
 
-                <div class="my-4 mx-10 md:mx-16 text-left">
+                <div class="md:mx-16 md:text-left">
+
+                    <h2 class="w-full font-black text-3xl tracking-widest uppercase py-2">Adentro del programa</h2>
+
                     @foreach($programme->descriptions as $description)
 
-                    <p class="mb-8 leading-normal font-bold text-lg transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110">
-                        <i class="fas fa-check-square fa-lg fa-fw text-green-600 pl-1"></i>
+                    <p class="my-8 leading-normal font-bold text-lg transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110">
+                        <i class="fas fa-check fa-lg text-blue-800 pl-1"></i>
                          {{ $description->phrase }}
                     </p>
 
                     @endforeach 
 
+                </div>
+
+                <div class="md:mx-16 md:text-left">
+                    <a href="{{url($programme->lienTiendup) }}" ><button class="bg-julien-gris hover:bg-julien-red text-white font-semibold tracking-widest uppercase border border-white rounded shadow hover:shadow-lg py-2 px-8 mx-auto transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110" type="button">Pedir Ahora</button></a>
                 </div>                
 
-                <a href="{{url($programme->lienTiendup) }}" class="mx-auto"><button class="bg-julien-red text-white font-semibold tracking-widest uppercase border border-white rounded shadow hover:shadow-lg py-2 px-4 mx-auto transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110" type="button">Pedir Ahora</button></a>
+                
             </div>
         </div>        
 
@@ -120,8 +124,10 @@
 
     </div>
 
+    {{-- Testimonios block --}}
+
     <div class="w-full text-center">
-        <h2 class="w-full font-black text-3xl text-white bg-julien-gris border-b-1 text-center uppercase py-8">Testimonios</h2>
+        <h2 class="w-full font-black text-3xl text-center uppercase py-8">Testimonios</h2>
     </div>
     
     <div class="w-full mx-auto flex flex-col md:flex-row flex-wrap items-stretch justify-around text-justify">
