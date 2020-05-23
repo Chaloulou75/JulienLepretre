b80f60388630f6b0b27fde5e844fac127df50fe5
Chaloulou75 <c.jeandey@gmail.com>
2020-05-23 20:17:21 -0300

    img web jpg carousel 3

--- resources/views/layouts/accueil/carousel.blade.php
+++ resources/views/layouts/accueil/carousel.blade.php
@@ -2,7 +2,7 @@
 
     <carousel-component>
 
-        <img src="/img/nsta/uno.jpg" loading="auto" alt="insta" class="carousel-cell object-contain h-48 w-full md:w-1/4">
+        <img src="/img/insta/uno.jpg" loading="auto" alt="insta" class="carousel-cell object-contain h-48 w-full md:w-1/4">
         <img src="/img/insta/dos.jpg" loading="auto" alt="insta" class="carousel-cell object-contain h-48 w-full md:w-1/4">
         <img src="/img/insta/tres.jpg" loading="auto" alt="insta" class="carousel-cell object-contain h-48 w-full md:w-1/4">
         <img src="/img/insta/cuatro.jpg" loading="auto" alt="insta" class="carousel-cell object-contain h-48 w-full md:w-1/4">
