9bdaf28629107264c725ec49a5594c3d99692414
Chaloulou75 <c.jeandey@gmail.com>
2020-05-23 19:28:33 -0300

    img web jpg

--- resources/views/layouts/accueil/carousel.blade.php
+++ resources/views/layouts/accueil/carousel.blade.php
@@ -2,18 +2,18 @@
 
     <carousel-component>
 
-        <img src="/img/insta/uno.jpeg" loading="auto" alt="insta" class="carousel-cell object-contain h-48 w-full md:w-1/4">
-        <img src="/img/insta/dos.jpeg" loading="auto" alt="insta" class="carousel-cell object-contain h-48 w-full md:w-1/4">
-        <img src="/img/insta/tres.jpeg" loading="auto" alt="insta" class="carousel-cell object-contain h-48 w-full md:w-1/4">
-        <img src="/img/insta/cuatro.jpeg" loading="auto" alt="insta" class="carousel-cell object-contain h-48 w-full md:w-1/4">
-        <img src="/img/insta/cinco.jpeg" loading="auto" alt="insta" class="carousel-cell object-contain h-48 w-full md:w-1/4">
-        <img src="/img/insta/seis.jpeg" loading="auto" alt="insta" class="carousel-cell object-contain h-48 w-full md:w-1/4">
-        <img src="/img/insta/siete.jpeg" loading="auto" alt="insta" class="carousel-cell object-contain h-48 w-full md:w-1/4">
-        <img src="/img/insta/ocho.jpeg" loading="auto" alt="insta" class="carousel-cell object-contain h-48 w-full md:w-1/4">
-        <img src="/img/insta/nueve.jpeg" loading="auto" alt="insta" class="carousel-cell object-contain h-48 w-full md:w-1/4">
-        <img src="/img/insta/dies.jpeg" loading="auto" alt="insta" class="carousel-cell object-contain h-48 w-full md:w-1/4">
-        <img src="/img/insta/once.jpeg" loading="auto" alt="insta" class="carousel-cell object-contain h-48 w-full md:w-1/4">
-        <img src="/img/insta/doce.jpeg" loading="auto" alt="insta" class="carousel-cell object-contain h-48 w-full md:w-1/4">
+        <img src="/img/insta/uno.jpg" loading="auto" alt="insta" class="carousel-cell object-contain h-48 w-full md:w-1/4">
+        <img src="/img/insta/dos.jpg" loading="auto" alt="insta" class="carousel-cell object-contain h-48 w-full md:w-1/4">
+        <img src="/img/insta/tres.jpg" loading="auto" alt="insta" class="carousel-cell object-contain h-48 w-full md:w-1/4">
+        <img src="/img/insta/cuatro.jpg" loading="auto" alt="insta" class="carousel-cell object-contain h-48 w-full md:w-1/4">
+        <img src="/img/insta/cinco.jpg" loading="auto" alt="insta" class="carousel-cell object-contain h-48 w-full md:w-1/4">
+        <img src="/img/insta/seis.jpg" loading="auto" alt="insta" class="carousel-cell object-contain h-48 w-full md:w-1/4">
+        <img src="/img/insta/siete.jpg" loading="auto" alt="insta" class="carousel-cell object-contain h-48 w-full md:w-1/4">
+        <img src="/img/insta/ocho.jpg" loading="auto" alt="insta" class="carousel-cell object-contain h-48 w-full md:w-1/4">
+        <img src="/img/insta/nueve.jpg" loading="auto" alt="insta" class="carousel-cell object-contain h-48 w-full md:w-1/4">
+        <img src="/img/insta/dies.jpg" loading="auto" alt="insta" class="carousel-cell object-contain h-48 w-full md:w-1/4">
+        <img src="/img/insta/once.jpg" loading="auto" alt="insta" class="carousel-cell object-contain h-48 w-full md:w-1/4">
+        <img src="/img/insta/doce.jpg" loading="auto" alt="insta" class="carousel-cell object-contain h-48 w-full md:w-1/4">
                           
     </carousel-component>
 </div>
