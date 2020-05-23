14286896291e67637cd6346696f66c30e94f7210
Chaloulou75 <c.jeandey@gmail.com>
2020-05-22 21:12:50 -0300

    tag manager -

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,13 +7,6 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="description" content="Consejos y ejercicios para un cuerpo sano - Julien Leprêtre">
     <title>{{ config('app.name', 'Julien Leprêtre') }}</title>
-    <!-- Google Tag Manager -->
-    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-    })(window,document,'script','dataLayer','GTM-N6NVVKQ');</script>
-    <!-- End Google Tag Manager -->  
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
     <!-- Fonts -->
@@ -22,12 +15,7 @@
     <link rel="shortcut icon" href="{{ asset('favicon.ico') }}">    
     <link href="{{ asset('css/app.css') }}" href="/css/app.css" rel="stylesheet">
 </head>
-<body class="bg-white">
-    <!-- Google Tag Manager (noscript) -->
-    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-N6NVVKQ"
-    height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-    <!-- End Google Tag Manager (noscript) -->
-    
+<body class="bg-white">   
     <div id="app" class="italic text-gray-900">
 
         <navbar-component></navbar-component>
