588bc60e2eefe8d21d492ccdcd1e124a5d2f9f54
Chaloulou75 <c.jeandey@gmail.com>
2020-09-15 20:50:49 -0300

    update redes

--- resources/views/layouts/accueil/redes.blade.php
+++ resources/views/layouts/accueil/redes.blade.php
@@ -15,13 +15,13 @@
     <div class="w-full text-sm font-semibold text-center md:w-1/3">
 
         <div class="flex-1 pb-4 text-sm">
-            <span class="text-2xl font-bold"><i class="mr-2 fas fa-users fa-2x text-julien-red"></i> 111,000 </span>
+            <span class="text-2xl font-bold"><i class="mr-2 fas fa-users fa-2x text-julien-red"></i> 121,000 </span>
             </br>
             Seguidores en Instagram
         </div>
 
         <div class="flex-1 py-2 text-sm">
-            <span class="text-2xl font-bold"><i class="mr-2 fas fa-photo-video fa-2x text-julien-red"></i> +390 </span>
+            <span class="text-2xl font-bold"><i class="mr-2 fas fa-photo-video fa-2x text-julien-red"></i> +420 </span>
             </br>
             Videos en las redes sociales
         </div>
